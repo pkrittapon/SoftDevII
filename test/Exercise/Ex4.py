bde88ddefa8d4352091844d7935b29533c430d89
lnWZA00X <107536606+lnWZA00X@users.noreply.github.com>
2022-12-19 21:13:21 +0700

    Ex4 fix

--- test/Exercise/Ex4.py
+++ test/Exercise/Ex4.py
@@ -1,10 +1,6 @@
 def duplicate(array):
-    check = False
     for i in range(len(array)):
         for j in range(i+1,len(array)):
             if array[i] == array[j]:
-                check = True
-                break   
-        if check:
-            return "Duplicate"          
+                return "Duplicate"          
     return "Not Duplicate"
