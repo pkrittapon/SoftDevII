dda609e5da44d14a413339ee554921387588139c
lnWZA00X <107536606+lnWZA00X@users.noreply.github.com>
2022-12-24 19:07:27 +0700

    delete

--- test/Exercise/Ex2.py
+++
@@ -1,4 +0,0 @@
-def plane(n):
-    if n == 0:
-        return 1
-    return plane(n-1) + n
--- test/Exercise/Ex4.py
+++
@@ -1,9 +0,0 @@
-def duplicate(array):#หาชุดตัวเลขซ้ำในลิสต์
-    dup_item = []
-    for i in range(len(array)):
-        for j in range(i+1,len(array)):
-            if array[i] == array[j] and not array[i] in dup_item:#เพิ่มตัวเลขที่ซ้าเข้าไปในชุดคำตอบเมื่อในชุดคำตอบไม่มีตัวเลขนี้อยู่
-                dup_item.append(array[i])  
-    if len(dup_item) == 0:        
-        return "Not Duplicate"
-    return dup_item
